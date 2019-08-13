dbaf8e9681e4e44763aec64b2a5ea4e0ad572901
Marek <michalskimarek89@gmail.com>
2019-08-13 19:15:14 +0200

    add user communication and time interval

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer");
 const CREDS = require("./creds");
+const readlineSync = require("readline-sync");
 
 const USERNAME_SELECTOR = "#username";
 const PASSWORD_SELECTOR = "#password";
@@ -10,6 +11,14 @@ const SELECT_ALL_SELECTOR = "#contact-select-checkbox";
 const ACCEPT_SELECTOR = ".self-focused .ember-view > div > button:nth-child(3)";
 
 async function run() {
+  console.log("Hi, I'll accept all LinkedIn invitations on your behalf!");
+  const userName = readlineSync.question("May I have your LOGIN? ");
+  const userPassword = readlineSync.question(
+    "Will you tell me your PASSWORD? "
+  );
+  const userInterval = readlineSync.question(
+    "How often should I check the invitations? (minutes)"
+  );
   const browser = await puppeteer.launch({
     headless: false
   });
@@ -18,23 +27,28 @@ async function run() {
   await page.goto("https://www.linkedin.com/login");
 
   await page.click(USERNAME_SELECTOR);
-  await page.keyboard.type(CREDS.username);
+  await page.keyboard.type(userName);
 
   await page.click(PASSWORD_SELECTOR);
-  await page.keyboard.type(CREDS.password);
+  await page.keyboard.type(userPassword);
 
   await page.click(BUTTON_SELECTOR);
+
   await page.goto("https://www.linkedin.com/mynetwork/invitation-manager/");
 
-  try {
-    await page.waitForSelector(INVITATION_SELECTOR, {
-      timeout: 1000
-    });
-    await page.click(SELECT_ALL_SELECTOR);
-    //await page.click(ACCEPT_SELECTOR);
-  } catch (error) {
-    console.log("The element didn't appear.");
-  }
+  setInterval(async function() {
+    try {
+      await page.waitForSelector(INVITATION_SELECTOR, {
+        timeout: 1000
+      });
+      await page.click(SELECT_ALL_SELECTOR);
+      await page.click(ACCEPT_SELECTOR);
+    } catch (error) {
+      console.log(
+        `No invitations, I'll check them again in ${userInterval} minutes`
+      );
+    }
+  }, userInterval * 60000);
 
   browser.close();
 }
